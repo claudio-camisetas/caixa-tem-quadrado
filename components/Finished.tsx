9e38ebda11301eaed67c69f8bb837dbe70909018
juritribuna <158317146+juritribuna@users.noreply.github.com>
2024-02-13 13:17:59 -0300

    refactor: change phone number

--- components/Finished.tsx
+++ components/Finished.tsx
@@ -9,7 +9,7 @@ import logo from '../images/logo.png'
 
 export default function Finished() {
   const openWhatsapp = () => {
-    location.href = 'https://api.whatsapp.com/send?phone=5516996981343&text=Solicitar%20empr%C3%A9stimo!'
+    location.href = 'https://api.whatsapp.com/send?phone=5524999673278&text=Solicitar%20empr%C3%A9stimo!'
   }
 
   return (
